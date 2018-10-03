8f796687351beac3fbc2e64a7781970ed92da4b5
CodeTurret <mushfiq.fuad@gmail.com>
2018-10-03 12:43:15 +0600

    Add, how to pass parameter through constructor

--- OOP_with_php/index2.php
+++ OOP_with_php/index2.php
@@ -1,6 +1,7 @@
 <?php
 
   include 'staticClass.php';
+  include 'pass_parameter_in_constructor.php';
 
 ?>
 <!DOCTYPE html>
@@ -22,5 +23,18 @@ echo "<br/>";
 echo staticClass::staticMethod2()."inside another static method";
 
 
+
+echo "<br>";
+echo "<br>";
+/*
+	Initialise object properties with constructor.
+	Instead of predefined value, here we give value first, then
+	the values go through the constrcutor method of the class.
+
+*/
+
+	$users1 = new Users('Abu','Zabbar','Mia',23,'Fishing');	
+
+	echo $users1->fullName();
 
 ?>
